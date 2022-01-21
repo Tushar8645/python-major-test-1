8f59d92e6d84e132fe62edf527703f6957e21228
Tushar Srivastava <srivastavatushar29@gmail.com>
2022-01-21 11:30:52 +0530

    'major-1'

---
+++ question_3.py
@@ -0,0 +1,36 @@
+# Write a Python program to create a word guessing game kind of like Hangman. Program should do the following operations:
+
+# a) Program chooses random word from list of words.
+
+# b) Limited guesses before game ends.
+
+# c) Addresses repeats when guessing.
+
+# d) Displays progress after each guess
+
+import random
+
+words = ['american', 'restaurant', 'programming', 'computer']
+random_word = random.choice(words)
+count = 0
+attempt_list = []
+
+while True:
+    guess = str(input('Guess a letter: ')).lower()
+
+    if guess not in attempt_list:
+        if guess in random_word:
+            print('Its in the word')
+        else:
+            count += 1
+            print('Not in word, attemps: {}'.format(count))
+
+            if count >= 5:
+                print('\nYou have reached max attempts')
+                print('Sorry, but hangman died! You lose')
+                break
+    else:
+        print('You already guessed {}. Try again'.format(guess))
+        print(set(attempt_list))
+
+    attempt_list.append(guess)
